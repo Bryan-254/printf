168ad802077f4af521bbc0d0154c52da49bba6ca
UserBryan <bkk@gmail.com>
2023-10-16 06:07:21 -0700

    Task 13 functions

---
+++ task13_functions.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+int print_rev(va_list args)
+{
+	int x, count = 0;
+	char *s;
+
+	s = va_arg(args, char*);
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	for (x = count - 1 ; x >= 0 ; x--)
+	{
+		putchar(s[x]);
+	}
+
+	return (count);
+}
