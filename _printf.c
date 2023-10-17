207bef19cf7219e118ec01d112c4d4e005dd40bf
UserBryan <bkk@gmail.com>
2023-10-17 05:31:24 -0700

    _printf c file

--- _printf.c
+++ _printf.c
@@ -6,21 +6,21 @@
 
 /**
  * _printf - This function mimics the printf function
- * @format: This is the string with the format specifier
- * Return: The number of characters to be  printed
+ * @format: This is the string with format specifier
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	if (format != NULL)
 	{
 		int count = 0, x;
-		int (*b)(va_list);
+		int (*m)(va_list);
 		va_list args;
 
 		va_start(args, format);
 		x = 0;
 		if (format[0] == '%' && format[1] == '\0')
-			return (-2);
+			return (-1);
 		while (format != NULL && format[x] != '\0')
 		{
 			if (format[x] == '%')
@@ -32,9 +32,9 @@ int _printf(const char *format, ...)
 				}
 				else
 				{
-					b = get_funct(format[x + 1]);
-					if (b)
-						count += b(args);
+					m = get_funct(format[x + 1]);
+					if (m)
+						count += m(args);
 					else
 						count = _putchar(format[x]) + _putchar(format[x + 1]);
 					x += 2;
@@ -49,5 +49,5 @@ int _printf(const char *format, ...)
 		va_end(args);
 		return (count);
 	}
-	return (-2);
+	return (-1);
 }
