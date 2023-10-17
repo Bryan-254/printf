bc6ef762fa9ebdcfae0ce0a1d0ee3a3b32f82052
UserBryan <bkk@gmail.com>
2023-10-17 04:32:54 -0700

    Task 2 functions

--- task2_functions.c
+++ task2_functions.c
@@ -4,21 +4,35 @@
 
 /**
  * print_bin - This function prints binary
- * @numb: The unsigned int argument
+ * @arg: The argument
  * Return: total number of binary digits processed
  */
 
-int print_bin(int numb)
+int bin(va_list arg)
 {
-	if (numb == 0)
+	unsigned int a;
+	int  x, b;
+	int arr[100];
+
+	a = va_arg(arg, int);
+	x = 0;
+
+	if (a == 0)
 	{
-		return (0);
+		_putchar('0');
+		return (1);
 	}
 
-	int digit = numb % 2;
-	int count = 1 + print_bin(numb / 2);
+	while (a > 0)
+	{
+		arr[x] = a % 2;
+		a = a / 2;
+		x++;
+	}
 
-	printf("%d", digit);
-
-	return (count);
+	for (b = x - 1; b >= 0; b--)
+	{
+		_putchar(arr[b] + '0');
+	}
+	return (x);
 }
