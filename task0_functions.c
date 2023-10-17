d302200d782193ada5e1e40ebc4b07fd9042d121
UserBryan <bkk@gmail.com>
2023-10-17 13:36:29 -0700

    Task 0 functions

--- task0_functions.c
+++ task0_functions.c
@@ -3,12 +3,12 @@
 #include <stdarg.h>
 
 /**
- * print_c - This function prints character
+ * print_ch - This function prints character
  * @args: This is a character argument
  * Return: The number of characters to be printed
  */
 
-int print_c(va_list args)
+int print_ch(va_list args)
 {
 	int x;
 
@@ -17,25 +17,27 @@ int print_c(va_list args)
 }
 
 /**
- * print_s - This function prints a string
+ * print_st - This function prints a string
  * @args: This is a string argument
  * Return: The number of characters to be printed
  */
 
-int print_s(va_list args)
+int print_st(va_list args)
 {
 	int a, count = 0;
-	char *str;
+	char *s;
 
 	a = 0;
-	str = va_arg(args, char*);
+	s = va_arg(args, char*);
 
-	if (str == NULL)
-		str = "(null)";
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
 
 	while (str[a] != '\0')
 	{
-		_putchar(str[a]);
+		_putchar(s[a]);
 		a++;
 		count++;
 	}
@@ -43,21 +45,21 @@ int print_s(va_list args)
 }
 
 /**
- * print_percent - This function passes the percent sign
+ * print_perc - This function passes the percent sign
  * @args: This parameter is a string argument
  * Return: return the percent sign
  *
  */
 
-int print_percent(va_list args)
+int print_perc(va_list args)
 {
-	char *str;
+	char *s;
 
-	str = "%";
+	s = "%";
 
-	if (va_arg(args, int) == *str)
+	if (va_arg(args, int) == *s)
 	{
-		return (*str);
+		return (*s);
 	}
-	return (*str);
+	return (*s);
 }
